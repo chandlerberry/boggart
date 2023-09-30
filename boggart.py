b876c7d959bc0109d602340afbb4aae2e64eea22
Chandler Berry <chandler.r.berry@gmail.com>
2023-09-30 16:21:01 -0400

    download image and send file to channel, instead of chatting back the url

--- boggart.py
+++ boggart.py
@@ -1,10 +1,10 @@
-import asyncio
+import asyncio, io, json, requests
 import discord
-import json
 import openai
 
 from discord.ext import commands
 from jsonschema import validate, ValidationError
+from PIL import Image
 
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
@@ -24,8 +24,10 @@ def get_api_keys(api_keys, api_key_schema):
     return keys
 
 async def generate_img(prompt):
-    response = openai.Image.create(prompt=prompt, n=1, size='1024x1024')
-    return response['data'][0]['url']
+    image_request = openai.Image.create(prompt=prompt, n=1, size='1024x1024')
+    get_image = requests.get(image_request['data'][0]['url']).content
+    image = Image.open(io.BytesIO(get_image))
+    return image
 
 @bot.event  
 async def on_ready():
@@ -36,14 +38,19 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user or str(message.channel) != 'boggart':
         return
-    print(f"Prompt: \"{message.content}\"")
-    image = await generate_img(prompt=message.content)
-    await message.channel.send(image)
+    
+    generate = asyncio.create_task(generate_img(prompt=message.content)) 
+    print(f"Prompt from {message.author}: \"{message.content}\"")
+    await message.channel.send("Generating...")
 
-def main():
+    image = await generate
+
+    await message.channel.send(file=image)
+    
+async def main():
     keys = get_api_keys(api_keys='keys.json', api_key_schema='key_schema.json')
     openai.api_key=(keys["openai"])
     bot.run(keys["discordBot"])
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

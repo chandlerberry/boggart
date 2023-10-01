28ed6cb3330e2ca6a04c2e0a73fd70e7b745e799
Chandler Berry <chandler.r.berry@gmail.com>
2023-10-01 19:42:26 -0400

    images are now saved in memory before being uploaded to discord's cdn

--- boggart.py
+++ boggart.py
@@ -1,14 +1,15 @@
-import asyncio, io, json, requests
+import aiohttp, io, json
 import discord
 import openai
+# import pendulum <- for better image upload names
 
 from discord.ext import commands
 from jsonschema import validate, ValidationError
-from PIL import Image
 
+# now = pendulum.now() <- for better image upload names
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
-async def get_api_keys(api_keys, api_key_schema):
+def get_api_keys(api_keys, api_key_schema) -> dict:
     with open(api_keys, 'r') as keys_file:
         keys = json.load(keys_file)
     
@@ -17,40 +18,42 @@ async def get_api_keys(api_keys, api_key_schema):
 
     try:
         validate(instance=keys, schema=schema)
-        print("Valid key schema")
+        print("\nVALID KEY SCHEMA\n")
     except ValidationError as e:
         print("Schema is invalid, check your \"keys.json\" file:", e)
 
     return keys
 
-async def generate_img(prompt):
+async def get_image_url(prompt) -> str:
     image_request = openai.Image.create(prompt=prompt, n=1, size='1024x1024')
-    get_image = requests.get(image_request['data'][0]['url']).content
-    image = Image.open(io.BytesIO(get_image))
-    return image
+    image_url = str(image_request['data'][0]['url'])
+    return image_url
 
 @bot.event  
 async def on_ready():
-    print("BOGGART CONNECTED")
+    print("\nBOGGART CONNECTED\n")
 
-# TODO: save image to memory and send image to server instead of url
 @bot.event
 async def on_message(message):
+    # TODO: add channel name to "keys" json
     if message.author == bot.user or str(message.channel) != 'boggart':
         return
-    
-    generate = asyncio.create_task(generate_img(prompt=message.content)) 
-    print(f"Prompt from {message.author}: \"{message.content}\"")
     await message.channel.send("Generating...")
-
-    image = await generate
-
-    await message.channel.send(file=image)
+    image_url = await get_image_url(prompt=message.content)
     
-async def main():
-    keys = await get_api_keys(api_keys='keys.json', api_key_schema='key_schema.json')
+    # maybe this is its own function?
+    async with aiohttp.ClientSession() as session:
+        async with session.get(image_url) as get_image:
+            if get_image.status != 200:
+                return await message.channel.send('Could not download file...')
+            image = io.BytesIO(await get_image.read())
+            # TODO: better image upload name
+            await message.channel.send(file=discord.File(fp=image, filename='dalle_image.png'))
+
+def main():
+    keys = get_api_keys(api_keys='keys.json', api_key_schema='key_schema.json')
     openai.api_key=(keys["openai"])
     bot.run(keys["discordBot"])
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()

fc27e30487589826a60d7b1473e3030178df9154
Chandler Berry <chandler.r.berry@gmail.com>
2023-10-26 08:12:49 -0400

    async updates to get_image and img command

--- boggart.py
+++ boggart.py
@@ -1,14 +1,13 @@
 import asyncio, aiohttp, io, json
 import discord
 import openai
-import pendulum
 
 from discord.ext import commands
 from jsonschema import validate, ValidationError
 from transformers import pipeline
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-summarizer = pipeline('summarization')
+summarizer = pipeline('summarization', model='')
 
 def get_api_keys(api_keys, api_key_schema) -> dict:
     with open(api_keys, 'r') as keys_file:
@@ -19,20 +18,20 @@ def get_api_keys(api_keys, api_key_schema) -> dict:
 
     try:
         validate(instance=keys, schema=schema)
-        print("\nVALID KEY SCHEMA\n")
     except ValidationError as e:
         print("Schema is invalid, check your \"keys.json\" file:", e)
 
     return keys
 
 async def get_image(prompt):
+    image_request = asyncio.to_thread(openai.Image.create(prompt=prompt, n=1, size='1024x1024'))
+
     try:
-        image_request = openai.Image.create(prompt=prompt, n=1, size='1024x1024')
+        result = await image_request
+        image_url = str(result['data'][0]['url'])
     except openai.error.InvalidRequestError as e:    
         return e
     
-    image_url = str(image_request['data'][0]['url'])
-
     async with aiohttp.ClientSession() as session:
         async with session.get(image_url) as get:
             if get.status != 200:
@@ -47,18 +46,16 @@ async def on_ready():
 async def img(ctx, *, prompt):
     if str(ctx.message.channel) != 'boggart':
         return
-    
+    prompt_summary = asyncio.to_thread(summarizer(prompt, max_length=1, min_length=1, do_sample=False))
     generate = asyncio.create_task(get_image(prompt))
-    dt = pendulum.parse(str(pendulum.now()))
     await ctx.send(f"Generating: \"{prompt}\"")
 
     image = await generate
     if isinstance(image, str):
         if "safety system" in image:
-            await ctx.send("Your prompt has been determined to be too based for the OpenAI safety system.")
+            await ctx.send(f"\"{prompt}\" has been determined to be too based for the OpenAI safety system.")
     elif isinstance(image, io.BytesIO):
-        prompt_summary = summarizer(prompt, max_length=10, min_length=2, do_sample=False)
-        print(f"Summary: {prompt_summary[0]['summary_text']}")
+        print(f"Summary: {await prompt_summary[0]['summary_text']}")
         await ctx.send(file=discord.File(fp=image, filename=f'dalle.png'))
     else:
         await(ctx.send(f"idk what this is, image request returned type: {type(image)}"))

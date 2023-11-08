cb8794b19113b9ea7ccd6d20c1bfaf9bbc7951d6
Chandler Berry <chandler.r.berry@gmail.com>
2023-11-08 07:44:39 -0500

    dalle 3 update functional

--- boggart.py
+++ boggart.py
@@ -2,6 +2,7 @@ import asyncio, aiohttp, io, json
 import discord
 import openai
 
+from openai import OpenAI
 from discord.ext import commands
 from jsonschema import validate, ValidationError
 # from transformers import pipeline
@@ -32,12 +33,21 @@ def get_api_keys(api_keys, api_key_schema) -> dict:
     # return summary[0]['text_summary']
 
 async def get_image(prompt):
-    result = openai.Image.create(model="dalle-3", prompt=prompt, n=1, size='1024x1024')
+    keys = get_api_keys(api_keys='keys.json', api_key_schema='key_schema.json')
+    client = OpenAI(api_key=keys['openai'])
+    result = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        n=1,
+        size='1024x1024',
+        quality="standard",
+    )
+    print(result)
 
     try:
-        image_url = str(result['data'][0]['url'])
-    except openai.error.InvalidRequestError as e:    
-        return e
+        image_url = result.data[0].url
+    except openai.OpenAIError as e:
+        return e.error
     
     async with aiohttp.ClientSession() as session:
         async with session.get(image_url) as get:
@@ -52,26 +62,25 @@ async def img(ctx, *, prompt):
     await ctx.send(f"Generating: \"{prompt}\"")
     # summary_thread = asyncio.to_thread(summarize_prompt(prompt))
     image_task = asyncio.create_task(get_image(prompt))
+    image = await image_task
 
-    image, summary = await asyncio.gather(
-        image_task,
-        # summary_thread
-    )
-
-    if isinstance(image, str):
-        if "safety system" in image:
-            await ctx.send(f"\"{prompt}\" has been determined to be too based for the OpenAI safety system.")
-    elif isinstance(image, io.BytesIO):
-        try:
-            print(f"Summary: {summary}")
-            await ctx.send(file=discord.File(fp=image, filename=f'dalle.png'))
-        except:
-            print("\nsummary's broke, fix\n\n")
-            await ctx.send("Chandler is big dumb dumb and bad at programming")
-    else:
-        await(ctx.send(f"idk what this is, image request returned type: {type(image)}"))
+    try:
+        await ctx.send(file=discord.File(fp=image, filename=f'dalle.png'))
+    except:
+        await ctx.send("somethings broke")
+    
+    # if isinstance(image, str):
+        # if "safety system" in image:
+            # await ctx.send(f"\"{prompt}\" has been determined to be too based for the OpenAI safety system.")
+    # elif isinstance(image, io.BytesIO):
+        # try:
+            # await ctx.send(file=discord.File(fp=image, filename=f'dalle.png'))
+        # except:
+            # print("\nsummary's broke, fix\n\n")
+            # await ctx.send("Chandler is big dumb dumb and bad at programming")
+    # else:
+        # await(ctx.send(f"idk what this is, image request returned type: {type(image)}"))
 
 if __name__ == "__main__":
     keys = get_api_keys(api_keys='keys.json', api_key_schema='key_schema.json')
-    openai.api_key=(keys['openai'])
     bot.run(keys['discordBot'])

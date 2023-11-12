7298e27fa1a3e054a2109837c2aeb4ec0aaf3ea0
Chandler Berry <chandler.r.berry@gmail.com>
2023-11-12 01:35:39 -0500

    hd image command

--- discordgpt/ImageGenerator.py
+++ discordgpt/ImageGenerator.py
@@ -1,4 +1,3 @@
-import asyncio
 import aiohttp
 import discord
 import io
@@ -14,7 +13,7 @@ class ImageGenerator(commands.Cog):
     async def img_test(self, ctx):
         await ctx.send('hello')
 
-    async def generate_image(self, prompt):
+    async def generate_image(self, prompt, image_size, image_quality):
         keys = KeyLoader('keys.json')
         client = OpenAI(
             api_key=str(keys.openai)
@@ -23,8 +22,8 @@ class ImageGenerator(commands.Cog):
             model="dall-e-3",
             prompt=prompt,
             n=1,
-            size="1024x1024",
-            quality="standard",
+            size=image_size,
+            quality=image_quality,
         )
         print(f"\n{result.data[0].revised_prompt}\n")
         return result.data[0].url
@@ -39,7 +38,35 @@ class ImageGenerator(commands.Cog):
             return
         await ctx.send(f"Generating: \"{prompt}\"")
         try:
-            image_url = await self.generate_image(prompt)
+            image_url = await self.generate_image(prompt, '1024x1024', 'standard')
+        except Exception as e:
+            await ctx.send(f"Error generating image: {e}")
+            return
+        
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(image_url) as response:
+                    if response.status != 200:
+                        await ctx.send("Failed to download the image.")
+                        return
+                    
+                    image_data = await response.read()
+                    image = io.BytesIO(image_data)
+                    filename = f"{ctx.message.author.display_name}.png"
+
+                    await ctx.send(file=discord.File(fp=image, filename=filename))
+
+        except Exception as e:
+            await ctx.send(f"Error sending image: {e}")
+
+    @commands.command()
+    async def img_hd(self, ctx, *, prompt):
+        if str(ctx.message.channel) != 'boggart' and str(ctx.message.author) != 'chndlr':
+            await ctx.send("Tell CHBE he needs more money")
+            return
+        await ctx.send(f"Generating HD Image: \"{prompt}\"")
+        try:
+            image_url = await self.generate_image(prompt, '1792x1024', 'hd')
         except Exception as e:
             await ctx.send(f"Error generating image: {e}")
             return

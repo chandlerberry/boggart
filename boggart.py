3fd87298606e0142b4da6d23bca56f3527a69d35
Chandler Berry <chandler.r.berry@gmail.com>
2023-12-09 14:28:45 -0500

    main file initializes backblaze environment vars

--- boggart.py
+++ boggart.py
@@ -1,10 +1,12 @@
 import asyncio
 import discord
+import os
 from discordgpt import KeyLoader
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
+# "Cogs" are weird and I wish I could do this differently, but here we are
 async def load_cogs():
     await bot.load_extension('discordgpt.ImageGenerator')
 
@@ -13,7 +15,8 @@ async def on_ready():
     print("\nBoggart Connected\n")
 
 async def main():
-    keys = KeyLoader('keys.json')
+    keys = KeyLoader(keys_file="keys.yaml",schema_file="schema.json")
+    os.environ.update(keys["backblaze"])
     await load_cogs()
     await bot.start(keys.discordBot)
 

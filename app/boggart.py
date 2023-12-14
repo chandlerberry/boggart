6d935582d8ca147e4a63e1c9116695c80ba79727
Chandler Berry <chandler.r.berry@gmail.com>
2023-12-14 00:34:27 -0500

    functionality of b2 upload validated

--- app/boggart.py
+++ app/boggart.py
@@ -1,13 +1,11 @@
 import asyncio
 import discord
 import os
-import uuid
 from discordgpt import KeyLoader
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-# "Cogs" are weird and I wish I could do this differently, but here we are
 async def load_cogs():
     await bot.load_extension('discordgpt.ImageGenerator')
 
@@ -16,9 +14,9 @@ async def on_ready():
     print("\nBoggart Connected\n")
 
 async def main():
-    keys = KeyLoader(keys_file='/config/keys.yaml',
-                     schema_file='/config/schema.json')
-    
+    keys = KeyLoader(keys_file='/config/staging.yaml',
+                    schema_file='/config/schema.json')
+
     os.environ["OPENAI_API_KEY"] = keys.openai
     os.environ["AWS_ENDPOINT_URL"] = keys.backblaze["endpoint_url"]
     os.environ["AWS_ACCESS_KEY_ID"] = keys.backblaze["application_key_id"]

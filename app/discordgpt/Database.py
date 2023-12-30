eb0830d64fa92829604d7023a2f280a48800a9d3
Chandler Berry <chandler.r.berry@gmail.com>
2023-12-29 23:13:08 -0500

    pgadmin no longer need functions to query database in test scripts

--- app/discordgpt/Database.py
+++ app/discordgpt/Database.py
@@ -28,7 +28,7 @@ class ImageDatabase:
 
         await conn.close()
 
-    async def store_generated_image(self, b2_filename: str, username: str, prompt: str, caption: str):
+    async def store_generated_image(self, b2_link: str, username: str, prompt: str, caption: str):
         """
         Store a reference to the generated image in the Boggart database
         """
@@ -41,48 +41,17 @@ class ImageDatabase:
         
         username_from_db = await conn.fetchval('SELECT UserID FROM Users WHERE Username = $1', username)
 
+        # TODO: if user does not exist, create them in the database here
+
         if username_from_db:
             await conn.execute('''
                 INSERT INTO GeneratedImages (ImageLink, TimeCreated, UserID, Prompt, Caption)
                 VALUES ($1, CURRENT_TIMESTAMP AT TIME ZONE 'UTC', $2, $3, $4)
-            ''', b2_filename, username_from_db, prompt, caption)
+            ''', b2_link, username_from_db, prompt, caption)
         else:
             raise Exception("Error inserting image into database")
 
         await conn.close()
-
-    async def get_all_users(self):
-        """
-        Returns a list of all users
-        """
-        conn = await asyncpg.connect(user=self.pg_username,
-                                     password=self.pg_password,
-                                     database=self.pg_database,
-                                     host=self.pg_host,
-                                     port=self.pg_port)
-            
-        data = await conn.fetch('SELECT * FROM Users')
-
-        for r in data:
-            print(f"{r['username']}: {r['userid']}")
-
-        await conn.close()
-
-    async def get_all_images(self):
-        """
-        Return a list of all database records of all images
-        """
-        conn = await asyncpg.connect(user=self.pg_username,
-                                     password=self.pg_password,
-                                     database=self.pg_database,
-                                     host=self.pg_host,
-                                     port=self.pg_port)
-        
-        data = await conn.fetch('SELECT * FROM GeneratedImages')
-
-        await conn.close()
-        
-        return data
 
     async def get_all_user_images(self, username:str):
         """

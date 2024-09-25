2275b490e3eb4adbfe964238a2721eacef80bc69
Chandler Berry <chandler.r.berry@gmail.com>
2024-09-24 23:17:19 -0400

    refactor bot and db

--- bot/boggart/boggart.py
+++ bot/boggart/boggart.py
@@ -8,29 +8,26 @@ import discord
 from discord.ext import commands
 
 class Boggart(commands.Bot):
-    def __init__(
-        self,
-        *args,
-        initial_extensions: List[str],
-        db_pool: asyncpg.Pool,
-        **kwargs,
-    ):
+    def __init__(self, *args, initial_extensions: List[str], db_pool: asyncpg.Pool, **kwargs,):
+
         super().__init__(*args, **kwargs)
+        
         self.initial_extensions = initial_extensions
         self.db_pool = db_pool
         self.command_prefix='!'
         self.logger = logging.getLogger('discord')
 
     async def setup_hook(self) -> None:
-        """
-        Load anything that should be in memory prior to handling events
-        """
+        """Load anything that should be in memory prior to handling events"""
+
         # Load extensions prior to sync to ensure interactions defined in those extensions are synced as well
         for extension in self.initial_extensions:
             await self.load_extension(extension)
             self.logger.info(f'{extension} loaded')
 
 async def main():
+
+    # todo: remove all this secret bs
     get_secret = lambda secret_file: open(f"/run/secrets/{secret_file}", 'r').read()
 
     date_format = '%Y-%m-%d %H:%M:%S'
@@ -43,29 +40,21 @@ async def main():
     discord_logger.setLevel(logging.INFO)
     discord_logger.addHandler(stream_handler)
     
+    # todo: load env vars from dotenv library, and pass as kwargs to connection pool
     pg_user = get_secret('postgres_username')
     pg_pass = get_secret('postgres_password')
     pg_host = get_secret('postgres_host')
     pg_port = get_secret('postgres_port')
     pg_database = get_secret('postgres_database')
 
-    async with asyncpg.create_pool(
-        user=pg_user,
-        password=pg_pass,
-        host=pg_host,
-        port=pg_port,
-        database=pg_database,
-        command_timeout=60
-    ) as pool:
-        exts = ['discordgpt.ImageGenerator']
+    async with asyncpg.create_pool(user=pg_user, password=pg_pass, host=pg_host, port=pg_port, database=pg_database, command_timeout=60) as pool:
+        
+        exts = ['ImageGenerator']
+        
         intents = discord.Intents.default()
         intents.message_content = True
-        async with Boggart(
-            commands.when_mentioned,
-            db_pool=pool,
-            initial_extensions=exts,
-            intents=intents,
-        ) as bot:
+        
+        async with Boggart(commands.when_mentioned, db_pool=pool, initial_extensions=exts, intents=intents) as bot:
             await bot.start(get_secret('discord_bot_key'))
 
 asyncio.run(main())
--- docs/environment.md
+++ docs/environment.md
@@ -1,7 +1,7 @@
 # Environment Configuration
 
-## Environment Variables
-### Bot
+## Bot
+### Docker Container Environment Vars
 - openai_api_key
 - openai_dalle_model
 - openai_dalle_image_size
@@ -18,7 +18,28 @@
 - postgres_host
 - postgres_port
 
-### Postgres
+## Postgres
+### Schema
+```sql
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS Users (
+    UserID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Username VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS GeneratedImages (
+    ImageID SERIAL PRIMARY KEY,
+    ImageLink VARCHAR(255) UNIQUE NOT NULL,
+    TimeCreated TIMESTAMPTZ NOT NULL,
+    UserID UUID NOT NULL,
+    Prompt TEXT NOT NULL,
+    Caption TEXT NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+```
+
+### Docker Environment Variables
 - postgres_username
 - postgres_password
 - postgres_database

2197f7dd75bdeae0b94f47940a3ead3b3fef5efe
Chandler Berry <chandler.r.berry@gmail.com>
2024-10-26 17:55:27 -0400

    db connection

--- boggart/main.py
+++ boggart/main.py
@@ -10,9 +10,7 @@ from discord.ext import commands
 
 class Boggart(commands.Bot):
     def __init__(self, *args, initial_extensions: List[str], db_pool: asyncpg.Pool, **kwargs,):
-
         super().__init__(*args, **kwargs)
-        
         self.initial_extensions = initial_extensions
         self.db_pool = db_pool
         self.command_prefix='!'
@@ -20,8 +18,6 @@ class Boggart(commands.Bot):
 
     async def setup_hook(self) -> None:
         """Load anything that should be in memory prior to handling events"""
-
-        # Load extensions prior to sync to ensure interactions defined in those extensions are synced as well
         for extension in self.initial_extensions:
             await self.load_extension(extension)
             self.logger.info(f'{extension} loaded')
@@ -37,16 +33,20 @@ async def main():
     discord_logger.setLevel(logging.INFO)
     discord_logger.addHandler(stream_handler)
 
+    pg_username = os.getenv('POSTGRES_USERNAME')
+    pg_password = os.getenv('POSTGRES_PASSWORD')
+    pg_host = os.getenv('POSTGRES_HOST')
+
     async with asyncpg.create_pool(
-        user=os.getenv('POSTGRES_USERNAME'),
-        password=os.getenv('POSTGRES_PASSWORD'),
-        host=os.getenv('POSTGRES_HOST'),
+        user=pg_username,
+        password=pg_password,
+        host=pg_host,
         port=5432,
-        database=os.getenv('POSTGRES_DATABASE'),
-        command_timeout=60) as pool:
-        
+        database='boggart',
+        command_timeout=60
+    ) as pool:
+
         exts = ['ImageGenerator']
-        
         intents = discord.Intents.default()
         intents.message_content = True
         

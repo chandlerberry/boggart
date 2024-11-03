53546cb5744b11eebd7b9cd57606b7633ed4e107
Chandler Berry <58996908+chandlerberry@users.noreply.github.com>
2024-11-02 23:25:50 -0400

    ollama among other things (#10)
    * update dockerfile to py 3.12
    
    * update .gitignore
    
    * pyproject init
    
    * rm old ansible crap
    
    * begone dockerfile warning
    
    * ollama llm replies update
    
    * seperate docker-compose for local minio storage
    
    * small fixes and docs

---
+++ boggart/llm_brain.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+import ollama
+from jinja2 import Environment, FileSystemLoader, Template
+
+def llm_response(prompt: str) -> str:
+    '''Calls out to self-hosted Ollama for chat request. Currently, the model is set via the OLLAMA_MODEL environment variable'''
+    env = Environment(loader = FileSystemLoader('/prompt_templates'), trim_blocks=True, lstrip_blocks=True)
+    template: Template = env.get_template('safety_system.yaml.j2')
+    template_input: dict = {'user_prompt': prompt}
+
+    client = ollama.Client(host=os.getenv('OLLAMA_ENDPOINT_URL'))
+
+    chat = client.chat(
+        model=os.getenv('OLLAMA_MODEL'),
+        messages=yaml.safe_load(template.render(template_input)),
+        options={
+            'mirostat': 1,
+            'temperature': 1
+        },
+        stream=False
+    )
+
+    return chat['message']['content']
